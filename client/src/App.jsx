2d01675b613fb6793cefa3691c190cabb80d105e
Cremacious <chrismackall3@gmail.com>
2025-10-20 18:30:52 -0400

    Created pages for auth, chat, profile, and home. Created forms for login and sign up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ChatPage from './pages/ChatPage';
+import ProfilePage from './pages/ProfilePage';
+import AuthPage from './pages/AuthPage';
+import { Toaster } from 'react-hot-toast';
+
 function App() {
   return (
-    <>
-      <div className="bg-blue-100">Hello There</div>
-    </>
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/chat/:id" element={<ChatPage />} />
+      </Routes>
+      <Toaster />
+    </div>
   );
 }
 
