3e560dd83469275f10b320d0d8f83355d09920df
cremacious <chrismackall3@gmail.com>
2025-10-22 09:36:09 -0400

    Reformatted server.js

--- api/server.js
+++ api/server.js
@@ -1,32 +1,37 @@
 import express from 'express';
-import cors from 'cors';
 import dotenv from 'dotenv';
-import { connectDB } from './config/db.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import path from 'path';
+import { createServer } from 'http';
+
+
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import matchRoutes from './routes/matchRoutes.js';
 import messageRoutes from './routes/messageRoutes.js';
-import cookieParser from 'cookie-parser';
-import { createServer } from 'http';
+
+import { connectDB } from './config/db.js';
 import { initializeSocket } from './socket/socket.server.js';
-import path from 'path';
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+
 const app = express();
 const httpServer = createServer(app);
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+initializeSocket(httpServer);
+
+app.use(express.json({ limit: '10mb' }));
+app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
-
-initializeSocket(httpServer);
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
@@ -42,6 +47,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 httpServer.listen(PORT, () => {
+  console.log('Server started at this port:' + PORT);
   connectDB();
-  console.log('App running in port: ' + PORT);
 });
